49a51a83b163736685aba2323a158c6592378e45
Edris Q <edrisq@uchicago.edu>
2021-06-01 18:19:20 +0000

    Finish implementation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 
 from fasta.fasta import acc_to_ORFs
 from fasta.align import needleman_wunsch, smith_waterman
@@ -6,6 +7,9 @@ from fasta.align import needleman_wunsch, smith_waterman
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+
+f = None
+sources = None
 
 @app.route('/')
 def root():
@@ -18,15 +22,17 @@ def table_init():
 
     return render_template('init.html', seq1=seq1, seq2=seq2)
 
-@app.route('/table_fill', methods=['GET', 'POST'])
-def table_fill():
+@app.route('/table_opts', methods=['GET', 'POST'])
+def table_opts():
+    global f, sources
+
     seq1 = request.form['seq1']
     seq2 = request.form['seq2']
     index = request.form['index']
     algo = request.form['algo']
     matrix = request.form['matrix']
     gap = request.form['gap']
-
+    
     imax = int(index) // len(seq1) + 1
     jmax = int(index) % len(seq1) + 1
 
@@ -37,8 +43,85 @@ def table_fill():
 
     # return jsonify({'seq1' : seq1, 'seq2' : seq2, 'index' : index, 'algo' : algo, 'matrix' : matrix, 
     #                 'gap' : gap, 'f': f, 'sources': sources, 'imax': imax, 'jmax': jmax})
-    return render_template('fill.html', seq1=seq1, seq2=seq2, index=index, algo=algo, 
-                            matrix=matrix, gap=gap, f=f, sources=sources, imax=imax, jmax=jmax)
+    return render_template('fill.html', seq1=seq1, seq2=seq2, index=index, algo=algo, matrix=matrix, 
+                            gap=gap, f=f, sources=sources, imax=imax, jmax=jmax)
+
+@app.route('/table_fill', methods=['GET', 'POST'])
+def table_fill():
+    global f, sources
+
+    seq1 = request.form['seq1']
+    seq2 = request.form['seq2']
+    index = request.form['index']
+    algo = request.form['algo']
+    matrix = request.form['matrix']
+    gap = request.form['gap']
+    step = request.form['step']
+
+    if int(step) == -3:
+        index = 0
+    elif int(step) == -2:
+        index = 0 if int(index) < len(seq1) else int(index) - len(seq1)
+    elif int(step) == -1:
+        index = 0 if int(index) < 1 else int(index) - 1
+    elif int(step) == 1:
+        index = len(seq1) * len(seq2) - 1 if int(index) + 1 >= len(seq1) * len(seq2) else int(index) + 1
+    elif int(step) == 2:
+        index = len(seq1) * len(seq2) - 1 if int(index) + len(seq1) >= len(seq1) * len(seq2) else int(index) + len(seq1)
+    elif int(step) == 3:
+        index = len(seq1) * len(seq2) - 1
+    
+    imax = int(index) // len(seq1) + 1
+    jmax = int(index) % len(seq1) + 1
+
+    # return jsonify({'seq1' : seq1, 'seq2' : seq2, 'index' : index, 'algo' : algo, 'matrix' : matrix, 
+    #                 'gap' : gap, 'f': f, 'sources': sources, 'imax': imax, 'jmax': jmax})
+    return render_template('fill.html', seq1=seq1, seq2=seq2, index=index, algo=algo, matrix=matrix, 
+                            gap=gap, f=f, sources=sources, imax=imax, jmax=jmax, step=step)
+
+@app.route('/table_trace', methods=['GET', 'POST'])
+def table_trace():
+    global f, sources
+
+    seq1 = request.form['seq1']
+    seq2 = request.form['seq2']
+    index = request.form['index']
+    algo = request.form['algo']
+    matrix = request.form['matrix']
+    gap = request.form['gap']
+    step = request.form['step']
+
+    # trace_step = request.form['trace_step']
+    try:
+        icurr = int(request.form['icurr'])
+        jcurr = int(request.form['jcurr'])
+        source = sources[icurr][jcurr][0]
+        icurr = int(icurr) + int(source[0])
+        jcurr = int(jcurr)  + int(source[1])
+
+        if algo == 'sw' and f[icurr][jcurr] <= 1:
+            raise exception
+    except:
+        if algo == 'sw':
+            max_val = 0
+            for i, row in enumerate(f):
+                for j, val in enumerate(row):
+                    if val > max_val:
+                        max_val = val
+                        icurr, jcurr = (i, j)
+        else:
+            icurr, jcurr = (len(seq1), len(seq2))
+
+    # if int(trace_step) == -1:
+    #     next
+    # elif int(trace_step) == 1:
+    #     next
+
+    imax = len(seq1)
+    jmax = len(seq2)
+    
+    return render_template('fill.html', seq1=seq1, seq2=seq2, index=index, algo=algo, matrix=matrix, 
+                            gap=gap, f=f, sources=sources, imax=imax, jmax=jmax, step=step, icurr=icurr, jcurr=jcurr)
 
 @app.route('/orf_finder')
 def orf_root():
